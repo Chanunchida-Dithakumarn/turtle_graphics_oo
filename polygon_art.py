b72311f60dd318373e0a0d52528b3cb74698d4da
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2024-11-09 23:30:23 +0700

    completed all and correct EmbeddedPolygon class

--- polygon_art.py
+++ polygon_art.py
@@ -1,56 +1,6 @@
 import turtle
 import random
 
-# def draw_polygon(num_sides, size, orientation, location, color, border_size):
-#     turtle.penup()
-#     turtle.goto(location[0], location[1])
-#     turtle.setheading(orientation)
-#     turtle.color(color)
-#     turtle.pensize(border_size)
-#     turtle.pendown()
-#     for _ in range(num_sides):
-#         turtle.forward(size)
-#         turtle.left(360/num_sides)
-#     turtle.penup()
-#
-# def get_new_color():
-#     return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-#
-# turtle.speed(0)
-# turtle.bgcolor('black')
-# turtle.tracer(0)
-# turtle.colormode(255)
-#
-# # draw a polygon at a random location, orientation, color, and border line thickness
-# num_sides = random.randint(3, 5) # triangle, square, or pentagon
-# size = random.randint(50, 150)
-# orientation = random.randint(0, 90)
-# location = [random.randint(-300, 300), random.randint(-200, 200)]
-# color = get_new_color()
-# border_size = random.randint(1, 10)
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-#
-# # specify a reduction ratio to draw a smaller polygon inside the one above
-# reduction_ratio = 0.618
-#
-# # reposition the turtle and get a new location
-# turtle.penup()
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.left(90)
-# turtle.forward(size*(1-reduction_ratio)/2)
-# turtle.right(90)
-# location[0] = turtle.pos()[0]
-# location[1] = turtle.pos()[1]
-#
-# # adjust the size according to the reduction ratio
-# size *= reduction_ratio
-#
-# # draw the second polygon embedded inside the original
-# draw_polygon(num_sides, size, orientation, location, color, border_size)
-#
-# # hold the window; close it by clicking the window close 'x' mark
-# turtle.done()
-
 
 class Polygon:
     def __init__(self, num_sides, size, orientation, location, color, border_size):
@@ -79,6 +29,7 @@ class Polygon:
 
 def get_new_color():
     return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
 
 class PolygonArt():
     def __init__(self):
@@ -123,7 +74,7 @@ class PolygonArt():
                 art = EmbeddedPolygon(num_sides, size, orientation, location, color, border_size, num_levels, reduction_ratio)
                 art.draw()
             elif choice == 7:
-                num_sides = 4
+                num_sides = 5
                 art = EmbeddedPolygon(num_sides, size, orientation, location, color, border_size, num_levels, reduction_ratio)
                 art.draw()
             elif choice == 8:
@@ -140,7 +91,6 @@ class PolygonArt():
 
 class EmbeddedPolygon(Polygon):
     def __init__(self, num_sides, size, orientation, location, color, border_size, num_levels, reduction_ratio):
-        # super().__init__(self, num_sides, size, orientation, location, color, border_size)
         self.num_sides = num_sides
         self.size = size
         self.orientation = orientation
